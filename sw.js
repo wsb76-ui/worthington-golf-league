b2dbb319597b0b11336f86f19f226db19326c126
wsb76-ui <wsb76@hotmail.com>
2026-07-13 10:25:00 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'wgl-v1';
+const ASSETS = [
+  '/worthington-golf-league/',
+  '/worthington-golf-league/index.html',
+  '/worthington-golf-league/manifest.json',
+  '/worthington-golf-league/icon-192.png',
+  '/worthington-golf-league/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if (res && res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
